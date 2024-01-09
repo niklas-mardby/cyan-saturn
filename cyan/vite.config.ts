f19bae1fe192ce9fed97f5d3a4cc286c1a37faf3
Niklas Mårdby <niklas.mardby@lynkco.com>
2024-01-09 10:01:57 +0100

    Added svgr plugin

--- cyan/vite.config.ts
+++ cyan/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    svgr(),
     react(),
     federation({
       name: "cyan",
